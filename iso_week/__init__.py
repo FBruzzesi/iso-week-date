d5040351d28a57b9f5219b6b02f98f8ccf568ccc
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-08-15 15:01:16 +0200

    min week number, docstring and _validate

--- iso_week/__init__.py
+++ iso_week/__init__.py
@@ -1,6 +1,7 @@
 from importlib import metadata
 
-from iso_week.isoweek import IsoWeek, datetime_to_isoweek, isoweek_to_datetime
+from iso_week.isoweek import IsoWeek
+from iso_week.pandas_utils import datetime_to_isoweek, isoweek_to_datetime
 
 __title__ = __name__
 __version__ = metadata.version(__title__)
---
+++ iso_week/pandas_utils.py
@@ -0,0 +1,41 @@
+from typing import Union
+
+import pandas as pd
+
+
+def datetime_to_isoweek(
+    series: pd.Series, offset: Union[pd.Timedelta, int] = pd.Timedelta(days=0)
+) -> pd.Series:
+    """
+    Convert datetime series to IsoWeek series
+
+    Arguments:
+        series: datetime series
+        offset: offset in days or pd.Timedelta
+
+    Returns:
+        IsoWeek series
+    """
+    _offset = pd.Timedelta(days=offset) if isinstance(offset, int) else offset
+    return (series + _offset).dt.strftime("%G-W%V")
+
+
+def isoweek_to_datetime(
+    series: pd.Series,
+    offset: Union[pd.Timedelta, int] = pd.Timedelta(days=0),
+    weekday: int = 1,
+) -> pd.Series:
+    """
+    Convert IsoWeek series to datetime series
+
+    Arguments:
+        series: IsoWeek series
+        offset: offset in days or pd.Timedelta
+        weekday: weekday to use for conversion
+
+    Returns:
+        datetime series
+    """
+
+    _offset = pd.Timedelta(days=offset) if isinstance(offset, int) else offset
+    return pd.to_datetime(series + "-" + f"{weekday}", format="%G-W%V-%w") + _offset
