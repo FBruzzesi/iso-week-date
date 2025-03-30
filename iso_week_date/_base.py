5c9b1f7c9b04148d766670b7b7b4d80d984f5627
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-03-30 18:20:53 +0200

    perf: Skip string validation for dates

--- iso_week_date/_base.py
+++ iso_week_date/_base.py
@@ -212,7 +212,9 @@ class BaseIsoWeek(ABC):
         if not isinstance(_date, date):
             msg = f"Expected `date` type, found {type(_date)}"
             raise TypeError(msg)
-        return cls((_date - cls.offset_).strftime(cls._date_format))
+        new_instance = cls.__new__(cls)
+        new_instance.value_ = (_date - cls.offset_).strftime(cls._date_format)
+        return new_instance
 
     @classmethod
     def from_datetime(cls: type[Self], _datetime: datetime) -> Self:
@@ -221,7 +223,9 @@ class BaseIsoWeek(ABC):
             msg = f"Expected `datetime` type, found {type(_datetime)}"
             raise TypeError(msg)
 
-        return cls((_datetime - cls.offset_).strftime(cls._date_format))
+        new_instance = cls.__new__(cls)
+        new_instance.value_ = (_datetime - cls.offset_).strftime(cls._date_format)
+        return new_instance
 
     @classmethod
     def from_today(cls: type[Self]) -> Self:  # pragma: no cover
